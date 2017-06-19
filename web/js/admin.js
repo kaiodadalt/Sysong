a85ac647137ef2a2b956af0a360a4373f7d66aa7
Kaio Freitas Dadalton <kaiofreitas.dadalton@gmail.com>
2017-06-19 19:08:05 -0300

    Adicionado pagina Geral

--- web/js/admin.js
+++ web/js/admin.js
@@ -1,1 +1,12 @@
+var Admin = {};
+
+Admin.init = function (){
+    $("#all").delay(2000).fadeIn();
+};
+
 $(".button-collapse").sideNav();
+
+document.addEventListener("DOMContentLoaded", function(){
+    $('.preloader-background').delay(1700).fadeOut();
+    $('.preloader-wrapper').delay(1700).fadeOut();
+});
--- web/js/init.js
+++ web/js/init.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
     $('.slider').slider();
 });
+
+var Index = {};
+
+Index.init = function (){
+    $("#all").delay(2000).fadeIn();
+};
+
+$(".button-collapse").sideNav();
+
+document.addEventListener("DOMContentLoaded", function(){
+    $('.preloader-background').delay(1700).fadeOut();
+    $('.preloader-wrapper').delay(1700).fadeOut();
+});
